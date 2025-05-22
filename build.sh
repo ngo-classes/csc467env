8a86953bb0d60c0ef3d06d11c5122d0c71eebd22
Linh B Ngo <lbn28@drexel.edu>
2025-05-22 17:31:04 -0400

    refactor repository

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker image rm spark-master:latest || true
-docker image rm spark-worker:latest || true
-
-docker build -t spark-master:latest ./docker/master
-docker build -t spark-worker:latest ./docker/worker
--- docker/master-student/entrypoint.sh
+++ docker/master-student/entrypoint.sh
@@ -13,7 +13,6 @@ export SPARK_HOME=/opt/spark
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
 cd / && jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 --allow-root &
-code-server code-server --bind-addr 0.0.0.0:8088 &
 
 cd /opt/spark/bin && /opt/spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
 
