f990fa418df91a9ceaabf8dfc54fe9f2e02e2b2a
Linh Ngo <linh.b.ngo@gmail.com>
2025-06-25 21:40:34 -0400

    update Docker setup

--- docker/master-instructor/install.sh
+++ docker/master-instructor/install.sh
@@ -10,6 +10,6 @@ log_info "Master image"
 # install code-serve
 sh /build/code-server.sh --prefix=/usr/local/
 
-pip3 install jupyter-book mkdocs-material markdown-include mkdocs-table-reader-plugin mkdocs-jupyter
+pip3 install jupyter-book mkdocs-material markdown-include mkdocs-table-reader-plugin mkdocs-jupyter mkdocs-glightbox
 
 cp /build/entrypoint.sh /usr/local/bin/entrypoint.sh
